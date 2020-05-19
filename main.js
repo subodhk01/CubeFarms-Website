75e728d8e19a53479ccb35073abd94cb9bc4fdc8
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-05-20 01:05:39 +0530

    Navigation changes

---
+++ main.js
@@ -0,0 +1,31 @@
+var display = 0;
+function servicesDropdown(){
+    dropdown = $("#serviceDropdown");
+    if( dropdown.css("opacity")=="0" ){
+        $(".down-arrow svg").css("transform","rotate(-90deg)");
+        dropdown.css("z-index","1");
+        dropdown.animate({
+            top: "0px",
+            opacity: "1",
+        }, 300);
+    } else {
+        $(".down-arrow svg").css("transform","rotate(90deg)");
+        dropdown.animate({
+            top: "16px",
+            opacity: "0",
+            zIndex: "-2",
+        }, 300);
+    }
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.nav-item')) {
+        if( dropdown.css("opacity")!="0" ){
+            $(".down-arrow svg").css("transform","rotate(90deg)");
+            dropdown.animate({
+                top: "16px",
+                opacity: "0",
+                zIndex: "-2",
+            }, 300);
+        }
+    }
+  }

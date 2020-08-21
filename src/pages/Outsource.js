ae66619f62945170ee7d303f63ccdfd4e224cb77
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-08-21 19:12:35 +0530

    Added transition for background change for looking for something else section

--- src/pages/Outsource.js
+++ src/pages/Outsource.js
@@ -6,8 +6,9 @@ import OurStandards from '../components/OutSource/OurStandards'
 import OurServices from '../components/OutSource/OurProcess'
 import LookingForSomethingElse from '../components/OutSource/LookingForSomethingElse'
 
+import { AnimatedBg, Transition } from 'scroll-background';
+
 const Container = styled.div`
-    background: black;
     .hero-container {
         min-height: 100vh;
         display: flex;
@@ -29,35 +30,38 @@ export default function Outsource(){
                 <Header white full />
             </div>
             <div>
-                <div className="hero-container">
-                    <div className="container">
-                        <div className="p-3" style={{maxWidth: "600px"}}>
-                            <h1 className="font-39 text-white">
-                                WEB STUDIO
-                            </h1>
-                            <div className="color-primary font-14">
-                                Websites promote you 24/7. No employee will do that.
-                            </div>
-                            <div className="font-13" style={{color: "rgba(245,245,245,0.83)"}}>
-                                We design and develop with a belief that every website deserves to 
-                                have its own voice. We build each and every website from scratch 
-                                combining function, content structure, and beauty in a single package.
+                <AnimatedBg>
+                    <div className="hero-container">
+                        <div className="container">
+                            <div className="p-3" style={{maxWidth: "600px"}}>
+                                <h1 className="font-39 text-white">
+                                    WEB STUDIO
+                                </h1>
+                                <div className="color-primary font-14">
+                                    Websites promote you 24/7. No employee will do that.
+                                </div>
+                                <div className="font-13" style={{color: "rgba(245,245,245,0.83)"}}>
+                                    We design and develop with a belief that every website deserves to 
+                                    have its own voice. We build each and every website from scratch 
+                                    combining function, content structure, and beauty in a single package.
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="hero-container">
-                    <WhyChooseUs />
-                </div>
-                <div className="hero-container">
-                    <OurStandards />
-                </div>
-                <div className="hero-container">
-                    <OurServices />
-                </div>
-                <div className="hero-container" style={{background: "#fafeff"}}>
-                    <LookingForSomethingElse />
-                </div>
+                    <div className="hero-container">
+                        <WhyChooseUs />
+                    </div>
+                    <div className="hero-container">
+                        <OurStandards />
+                    </div>
+                    <div className="hero-container">
+                        <OurServices />
+                    </div>
+                    <Transition height="100px" from="#000000" to="#ffffff" />
+                    <div className="hero-container">
+                        <LookingForSomethingElse />
+                    </div>
+                </AnimatedBg>
             </div>
         </Container>
     )

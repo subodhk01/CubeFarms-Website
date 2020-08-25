fc682c027befb33c1915267b19bc348de6b644f6
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-08-25 20:32:57 +0530

    Changes in Static side bar section

--- src/components/StaticSidePanelSection.js
+++ src/components/StaticSidePanelSection.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
+import { AnimatedBg, Transition } from 'scroll-background';
+import VisibSensor from 'react-visibility-sensor'
 import { PRIMARY, PRIMARY_DARK } from '../utils/Colors'
 
 const data = [1,2,3,4]
@@ -47,67 +49,180 @@ const Container = styled.div`
         border-radius: 13px;
         background-color: ${PRIMARY};
         color: white;
-        font-weight: 800;
-        font-size: 1.1rem;
         text-shadow: 0px 0px 10px rgba(0,0,0,0.8);
-        opacity: 0.3;
     }
 `
 
 export default function StaticSidePanelSection() {
+    const [ tab, setTab ] = React.useState({})
+    const handlestep1 = (visibility) => {
+        console.log("step1")
+        if(visibility) setTab({step1: true})
+        else setTab({...tab, step1: false})
+    }
+    const handlestep2 = (visibility) => {
+        console.log("step2")
+        if(visibility) setTab({step2: true})
+        else setTab({...tab, step2: false})
+    }
+    const handlestep3 = (visibility) => {
+        console.log("step3")
+        if(visibility) setTab({step3: true})
+        else setTab({...tab, step3: false})
+    }
+    const handlestep4 = (visibility) => {
+        console.log("step4")
+        if(visibility) setTab({step4: true})
+        else setTab({...tab, step4: false})
+    }
     return (
         <Container>
-            <div className="row no-gutters justify-content-end position-relative">
-                <div className="left-panel p-3 h-100vh">
+            <div className="row no-gutters justify-content-end">
+                <div className="left-panel px-3 py-5 h-100vh">
                     <div className="heading text-center">
                         Our expertise
                     </div>
                     <div className="tabs py-5 my-2">
-                        <div className="tab p-3 active-tag">
+                        <div className={`tab p-3 my-2 ${tab.step1 ? "active-tag": ""}`}>
                             SEO
                         </div>
-                        <div className="tab p-3 my-2">
+                        <div className={`tab p-3 my-2 ${tab.step2 ? "active-tag": ""}`}>
                             Social Media Advertising
                         </div>
-                        <div className="tab p-3 my-2">
+                        <div className={`tab p-3 my-2 ${tab.step3 ? "active-tag": ""}`}>
                             Pay Per Click (PPC)
                         </div>
-                        <div className="tab p-3 my-2">
+                        <div className={`tab p-3 my-2 ${tab.step4 ? "active-tag": ""}`}>
                             Content Writing Services
                         </div>
                     </div>
                 </div>
-                <div className="col-12 col-md-8 right-panel p-3">
-                    { data.map((item, index) => 
-                        <div className="d-flex align-items-center h-100vh py-5">
-                            <div className="px-3 px-md-5">
-                                <div className="heading-bold">
-                                    <span style={{color: "aqua"}}>01</span><br />Landing Pages
-                                </div>
-                                <div className="pt-3 pb-5">
-                                    Single Page responsive websites designed with conversion goals in mind.
-                                </div>
-                                <div className="row gutters">
-                                    <div className="col-6">
-                                        <div className="py-4"><strong>Recommended Settings: </strong></div>
-                                        <ul>
-                                            <li>Single Page responsive design</li>
-                                            <li>Multilingual Options</li>
-                                            <li>Online form integration</li>
-                                            <li>CMS enabled</li>
-                                        </ul>
+                <div className="col-12 col-md-8 right-panel">
+                    <AnimatedBg>
+                            <div className="d-flex align-items-center h-100vh py-5">
+                                <VisibSensor onChange={handlestep1} partialVisibility={true} minTopValue={180}>
+                                    <div className="px-3 px-md-5">
+                                        <div className="heading-bold">
+                                            <span style={{color: "aqua"}}>01</span><br />Landing Pages
+                                        </div>
+                                        <div className="pt-3 pb-5">
+                                            Single Page responsive websites designed with conversion goals in mind.
+                                        </div>
+                                        <div className="row gutters">
+                                            <div className="col-6">
+                                                <div className="py-4"><strong>Recommended Settings: </strong></div>
+                                                <ul>
+                                                    <li>Single Page responsive design</li>
+                                                    <li>Multilingual Options</li>
+                                                    <li>Online form integration</li>
+                                                    <li>CMS enabled</li>
+                                                </ul>
+                                            </div>
+                                            <div className="col-6">
+                                                <div className="py-4"><strong>Good for: </strong></div>
+                                                <ul>
+                                                    <li>Signup page for Consultation service</li>
+                                                    <li>Promotional Campaign for new product or service</li>
+                                                </ul>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div className="col-6">
-                                        <div className="py-4"><strong>Good for: </strong></div>
-                                        <ul>
-                                            <li>Signup page for Consultation service</li>
-                                            <li>Promotional Campaign for new product or service</li>
-                                        </ul>
-                                    </div>
-                                </div>
+                                </VisibSensor>
                             </div>
-                        </div>
-                    )}
+                        <Transition height="100px" from={PRIMARY} to={PRIMARY_DARK} position={0.5} />
+                            <div className="d-flex align-items-center h-100vh py-5">
+                                <VisibSensor onChange={handlestep2} partialVisibility={true} minTopValue={180}>
+                                    <div className="px-3 px-md-5">
+                                        <div className="heading-bold">
+                                            <span style={{color: "aqua"}}>01</span><br />Landing Pages
+                                        </div>
+                                        <div className="pt-3 pb-5">
+                                            Single Page responsive websites designed with conversion goals in mind.
+                                        </div>
+                                        <div className="row gutters">
+                                            <div className="col-6">
+                                                <div className="py-4"><strong>Recommended Settings: </strong></div>
+                                                <ul>
+                                                    <li>Single Page responsive design</li>
+                                                    <li>Multilingual Options</li>
+                                                    <li>Online form integration</li>
+                                                    <li>CMS enabled</li>
+                                                </ul>
+                                            </div>
+                                            <div className="col-6">
+                                                <div className="py-4"><strong>Good for: </strong></div>
+                                                <ul>
+                                                    <li>Signup page for Consultation service</li>
+                                                    <li>Promotional Campaign for new product or service</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </VisibSensor>
+                            </div>
+                        <Transition height="100px" from={PRIMARY_DARK} to={PRIMARY} position={0.5} />
+                            <div className="d-flex align-items-center h-100vh py-5">
+                                <VisibSensor onChange={handlestep3} partialVisibility={true} minTopValue={180}>
+                                    <div className="px-3 px-md-5">
+                                        <div className="heading-bold">
+                                            <span style={{color: "aqua"}}>01</span><br />Landing Pages
+                                        </div>
+                                        <div className="pt-3 pb-5">
+                                            Single Page responsive websites designed with conversion goals in mind.
+                                        </div>
+                                        <div className="row gutters">
+                                            <div className="col-6">
+                                                <div className="py-4"><strong>Recommended Settings: </strong></div>
+                                                <ul>
+                                                    <li>Single Page responsive design</li>
+                                                    <li>Multilingual Options</li>
+                                                    <li>Online form integration</li>
+                                                    <li>CMS enabled</li>
+                                                </ul>
+                                            </div>
+                                            <div className="col-6">
+                                                <div className="py-4"><strong>Good for: </strong></div>
+                                                <ul>
+                                                    <li>Signup page for Consultation service</li>
+                                                    <li>Promotional Campaign for new product or service</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </VisibSensor>
+                            </div>
+                        <Transition height="100px" from={PRIMARY} to={PRIMARY_DARK} position={0.5} />
+                            <div className="d-flex align-items-center h-100vh py-5">
+                                <VisibSensor onChange={handlestep4} partialVisibility={true} minTopValue={180}>
+                                    <div className="px-3 px-md-5">
+                                        <div className="heading-bold">
+                                            <span style={{color: "aqua"}}>01</span><br />Landing Pages
+                                        </div>
+                                        <div className="pt-3 pb-5">
+                                            Single Page responsive websites designed with conversion goals in mind.
+                                        </div>
+                                        <div className="row gutters">
+                                            <div className="col-6">
+                                                <div className="py-4"><strong>Recommended Settings: </strong></div>
+                                                <ul>
+                                                    <li>Single Page responsive design</li>
+                                                    <li>Multilingual Options</li>
+                                                    <li>Online form integration</li>
+                                                    <li>CMS enabled</li>
+                                                </ul>
+                                            </div>
+                                            <div className="col-6">
+                                                <div className="py-4"><strong>Good for: </strong></div>
+                                                <ul>
+                                                    <li>Signup page for Consultation service</li>
+                                                    <li>Promotional Campaign for new product or service</li>
+                                                </ul>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </VisibSensor>
+                            </div>
+                    </AnimatedBg>
                 </div>
             </div>
         </Container>

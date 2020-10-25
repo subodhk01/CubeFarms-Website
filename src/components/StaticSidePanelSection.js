b7bee51699fb66bf648a2f420e65af927ba88c99
Subodh Verma <subodh.verma.min19@iitbhu.ac.in>
2020-10-25 22:08:38 +0530

    Minor changes in SidePanel, committing dur to local repo deletion

--- src/components/StaticSidePanelSection.js
+++ src/components/StaticSidePanelSection.js
@@ -51,35 +51,57 @@ const Container = styled.div`
         color: white;
         text-shadow: 0px 0px 10px rgba(0,0,0,0.8);
     }
+    .title-index {
+        font-size: 3.5rem;
+    }
+    .title-heading {
+        font-size: 2rem;
+    }
+    .mid-data{
+        font-size: 1.8rem;
+    }
+    .end-data {
+        font-size: 1.7rem;
+    }
+    @media(max-width: 767px){
+        .title-index {
+            font-size: 3rem;
+        }
+        .title-heading {
+            font-size: 1.7rem;
+        }
+        .mid-data{
+            font-size: 1.2rem;
+        }
+        .end-data {
+            font-size: 1.1rem;
+        }
+    }
 `
 
 export default function StaticSidePanelSection() {
     const [ tab, setTab ] = React.useState({})
     const handlestep1 = (visibility) => {
-        console.log("step1")
         if(visibility) setTab({step1: true})
         else setTab({...tab, step1: false})
     }
     const handlestep2 = (visibility) => {
-        console.log("step2")
         if(visibility) setTab({step2: true})
         else setTab({...tab, step2: false})
     }
     const handlestep3 = (visibility) => {
-        console.log("step3")
         if(visibility) setTab({step3: true})
         else setTab({...tab, step3: false})
     }
     const handlestep4 = (visibility) => {
-        console.log("step4")
         if(visibility) setTab({step4: true})
         else setTab({...tab, step4: false})
     }
     return (
         <Container>
             <div className="row no-gutters justify-content-end">
-                <div className="left-panel px-3 py-5 h-100vh">
-                    <div className="heading text-center">
+                <div className="left-panel px-3 py-10  h-100vh">
+                    <div className="heading text-center pt-5">
                         Our expertise
                     </div>
                     <div className="tabs py-5 my-2">
@@ -103,12 +125,15 @@ export default function StaticSidePanelSection() {
                                 <VisibSensor onChange={handlestep1} partialVisibility={true} minTopValue={180}>
                                     <div className="px-3 px-md-5">
                                         <div className="heading-bold">
-                                            <span style={{color: "aqua"}}>01</span><br />Landing Pages
+                                            <div className="title-index">01</div>
+                                            <div className="title-heading">
+                                                Landing Pages
+                                            </div>
                                         </div>
-                                        <div className="pt-3 pb-5">
+                                        <div className="pt-3 pb-5 mid-data">
                                             Single Page responsive websites designed with conversion goals in mind.
                                         </div>
-                                        <div className="row no-gutters">
+                                        <div className="row no-gutters end-data">
                                             <div className="col-6">
                                                 <div className="py-4"><strong>Recommended Settings: </strong></div>
                                                 <ul>
@@ -129,17 +154,20 @@ export default function StaticSidePanelSection() {
                                     </div>
                                 </VisibSensor>
                             </div>
-                        <Transition height="100px" from={PRIMARY} to={PRIMARY_DARK} position={0.5} />
+                        <Transition height="100px" from="#031E87" to="#5473FF" position={0.5} />
                             <div className="d-flex align-items-center h-100vh py-5">
                                 <VisibSensor onChange={handlestep2} partialVisibility={true} minTopValue={180}>
                                     <div className="px-3 px-md-5">
                                         <div className="heading-bold">
-                                            <span style={{color: "aqua"}}>01</span><br />Landing Pages
+                                            <div className="title-index">01</div>
+                                            <div className="title-heading">
+                                                Landing Pages
+                                            </div>
                                         </div>
-                                        <div className="pt-3 pb-5">
+                                        <div className="pt-3 pb-5 mid-data">
                                             Single Page responsive websites designed with conversion goals in mind.
                                         </div>
-                                        <div className="row no-gutters">
+                                        <div className="row no-gutters end-data">
                                             <div className="col-6">
                                                 <div className="py-4"><strong>Recommended Settings: </strong></div>
                                                 <ul>
@@ -160,17 +188,20 @@ export default function StaticSidePanelSection() {
                                     </div>
                                 </VisibSensor>
                             </div>
-                        <Transition height="100px" from={PRIMARY_DARK} to={PRIMARY} position={0.5} />
+                        <Transition height="100px" from="#5473FF" to="#9AAAC1" position={0.5} />
                             <div className="d-flex align-items-center h-100vh py-5">
                                 <VisibSensor onChange={handlestep3} partialVisibility={true} minTopValue={180}>
                                     <div className="px-3 px-md-5">
                                         <div className="heading-bold">
-                                            <span style={{color: "aqua"}}>01</span><br />Landing Pages
+                                            <div className="title-index">01</div>
+                                            <div className="title-heading">
+                                                Landing Pages
+                                            </div>
                                         </div>
-                                        <div className="pt-3 pb-5">
+                                        <div className="pt-3 pb-5 mid-data">
                                             Single Page responsive websites designed with conversion goals in mind.
                                         </div>
-                                        <div className="row no-gutters">
+                                        <div className="row no-gutters end-data">
                                             <div className="col-6">
                                                 <div className="py-4"><strong>Recommended Settings: </strong></div>
                                                 <ul>
@@ -191,17 +222,20 @@ export default function StaticSidePanelSection() {
                                     </div>
                                 </VisibSensor>
                             </div>
-                        <Transition height="100px" from={PRIMARY} to={PRIMARY_DARK} position={0.5} />
+                        <Transition height="100px" from="#9AAAC1" to="#FCC6C5" position={0.5} />
                             <div className="d-flex align-items-center h-100vh py-5">
                                 <VisibSensor onChange={handlestep4} partialVisibility={true} minTopValue={180}>
                                     <div className="px-3 px-md-5">
                                         <div className="heading-bold">
-                                            <span style={{color: "aqua"}}>01</span><br />Landing Pages
+                                            <div className="title-index">01</div>
+                                            <div className="title-heading">
+                                                Landing Pages
+                                            </div>
                                         </div>
-                                        <div className="pt-3 pb-5">
+                                        <div className="pt-3 pb-5 mid-data">
                                             Single Page responsive websites designed with conversion goals in mind.
                                         </div>
-                                        <div className="row no-gutters">
+                                        <div className="row no-gutters end-data">
                                             <div className="col-6">
                                                 <div className="py-4"><strong>Recommended Settings: </strong></div>
                                                 <ul>
